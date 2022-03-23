af6e71ec1de6626358034b35ca4571193bec3a3c
Patrick Brown <ptrckbrwn@icloud.com>
2022-03-23 02:51:38 -0700

    Create role join/leave commands

--- src/DeployCommands.ts
+++ src/DeployCommands.ts
@@ -5,14 +5,28 @@ import { Configuration } from './Configuration';
 
 const configuration = Configuration.getInstance();
 
-const commands = [
-    new SlashCommandBuilder()
-        .setName('example')
-        .setDescription('An example slash command.'),
-].map(command => command.toJSON());
+const command = new SlashCommandBuilder()
+        .setName('role')
+        .setDescription('Join or leave roles')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('join')
+                .setDescription('Join a role')
+                .addRoleOption(option =>
+                    option.setName('role')
+                        .setDescription('The role to join')
+                        .setRequired(true)))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('leave')
+                .setDescription('Leave a role')
+                .addRoleOption(option =>
+                    option.setName('role')
+                        .setDescription('The role to leave')
+                        .setRequired(true)));
 
 const rest = new REST({ version: '9' }).setToken(configuration.discordBotToken);
 
-rest.put(Routes.applicationCommands(configuration.discordClientId), { body: commands })
+rest.put(Routes.applicationCommands(configuration.discordClientId), { body: [command.toJSON()] })
     .then(() => console.log('Successfully registered application commands.'))
     .catch(console.error);
